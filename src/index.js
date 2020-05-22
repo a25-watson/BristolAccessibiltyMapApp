fc77a3d5e3f9e4823cdff2d9e4e929646220b113
a25-watson <Aiden2.Watson@live.uwe.ac.uk>
2020-05-22 07:04:27 +0100

    1.3 adding code comments

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
+/** 
+// Name: Aiden John Watson
+// Date: 01/05/2020
+// Application: BristolAccessibilityMapApp
+// Class: index.js
+
+// This is the index.js class for rendering react applications.
+// currently only the map application but being modular more apps can be added.
+// 
+*/
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Map from './components/Map';
 
+//
 class App extends React.Component 
 {
+    // creates a new in memory DOM and compares to the previous.
+    // allowing for updates to the browser.
 	render() 
 	{
 		return(
@@ -18,4 +32,5 @@ class App extends React.Component
 	}
 }
 
+// compares the html code and html element to display the specified element.
 ReactDOM.render(<App />, document.getElementById('app'));
